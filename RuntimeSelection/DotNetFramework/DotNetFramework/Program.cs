a836d6bf367da4475c01832551bf388296947c0a
Allan Chua <allanchua.officefiles@gmail.com>
2017-11-27 21:53:04 +0800

    .Net Runtime Selection Demo Apps
    Created applications that demonstrates the difference between image sizes produced by .Net Framework and .Net Core

---
+++ RuntimeSelection/DotNetFramework/DotNetFramework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
