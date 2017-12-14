0f23f23f1922df0b19c34cd08eca793da1e6c6e1
Allan Chua <allanchua.officefiles@gmail.com>
2017-12-15 00:14:45 +0800

    -Magic value content and samples

---
+++ SqlBooleanMagicValue/CodeSamples/BadTaxCalculator.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// Class used for calculating tax.
+/// </summary>
+public class BadTaxCalculator
+{
+    /// <summary>
+    /// Method used for calculating salary tax.
+    /// </summary>
+    /// <param name="salary">
+    ///     The salary that would be
+    ///     processed for tax calculation.
+    /// </param>
+    /// <returns>
+    ///     The tax that would be reduced from
+    ///     the provided salary.
+    /// </returns>
+	public float Execute(int salary)
+    {
+        return (salary * .30) - 125; // Bad code due to magic values
+    }
+}
---
+++ SqlBooleanMagicValue/CodeSamples/GoodTaxCalculator.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// Class used for calculating tax.
+/// </summary>
+public class GoodTaxCalculator
+{
+    private const float BASE_REDUCTION_VALUE = .30f;
+    private const int SOCIAL_SECURITY_CONTRIBUTIONS = 125;
+
+    /// <summary>
+    /// Method used for calculating tax that would be reduced salary.
+    /// </summary>
+    /// <param name="salary">
+    ///     The salary that would be
+    ///     processed for tax calculation.
+    /// </param>
+    /// <returns>
+    ///     The tax that would be reduced from
+    ///     the provided salary.
+    /// </returns>
+    public float Execute(int salary)
+    {
+        
+        return (salary * BASE_REDUCTION_VALUE) - SOCIAL_SECURITY_CONTRIBUTIONS;
+    }
+}
